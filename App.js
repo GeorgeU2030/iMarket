31b49295e4f6df8010b78bdcd2a786d1698e6ff7
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-24 17:44:42 -0500

    authentication_has_been_implemented

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import LoginScreen from './src/screens/LoginScreen';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function App() {
 
@@ -13,10 +16,18 @@ export default function App() {
     return <View><Text>Loading...</Text></View>;
   }
   return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
     <View className="flex-1  bg-white">
-      <LoginScreen/>
+     
+      <SignedIn>
+          <Text>You are Signed in</Text>
+      </SignedIn>
+      <SignedOut>
+          <LoginScreen />
+      </SignedOut>
       <StatusBar style="auto" />
     </View>
+    </ClerkProvider>
   );
 }
 
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,7 +1,31 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from '../../hooks/useWarmUpBrowser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+  useWarmUpBrowser();
+ 
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+ 
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+
   return (
     <View>
       <Image source={require('../../assets/images/loginimg.jpg')}
@@ -10,7 +34,7 @@ export default function LoginScreen() {
         <View className="p-8 rounded-t-3xl mt-[-20px] bg-white shadow-md">
             <Text className="font-semibold text-2xl" style={globalStyles.text}>iMarket</Text>
             <Text className="text-slate-800" style={globalStyles.text}>Your destination to discover and offer products securely and efficiently in our vibrant marketplace.</Text>
-            <TouchableOpacity onPress={()=>console.log("signin")} className="p-2 bg-[#42ad99] rounded-full mt-16">
+            <TouchableOpacity onPress={onPress} className="p-2 bg-[#42ad99] rounded-full mt-16">
             <Text className="text-center text-white text-lg" style={globalStyles.text}>Get Started</Text>
             </TouchableOpacity>
         </View>
