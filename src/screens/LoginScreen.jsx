10160f08bb03b6a751bb90dc52d59de6a4def025
GeorgeU2030 <georgeu2021@gmail.com>
2024-02-24 16:28:07 -0500

    getstarted_screen

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,17 +1,27 @@
-import { View, Text, Image, StyleSheet} from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
     <View>
-      <Image source={require('../../assets/images/loginimg.png')}
+      <Image source={require('../../assets/images/loginimg.jpg')}
       className="w-full h-[400px] object-cover"
       />
-        <View className="p-10">
-            <Text style={globalStyles.text}>iMarket</Text>
-            <Text>prueba diferencia</Text>
-            <Text style={globalStyles.text}>prueba diferencia</Text>
+        <View className="p-8 rounded-t-3xl mt-[-20px] bg-white shadow-md">
+            <Text className="font-semibold text-2xl" style={globalStyles.text}>iMarket</Text>
+            <Text className="text-slate-800" style={globalStyles.text}>Your destination to discover and offer products securely and efficiently in our vibrant marketplace.</Text>
+            <TouchableOpacity onPress={()=>console.log("signin")} className="p-2 bg-[#42ad99] rounded-full mt-16">
+            <Text className="text-center text-white text-lg" style={globalStyles.text}>Get Started</Text>
+            </TouchableOpacity>
         </View>
+            <View className="flex flex-row items-center justify-center mt-2">
+                <Image source={require('../../assets/images/icesilogo.jpg')}
+                className="w-20 h-20 rounded-full"
+                />
+            </View>
+            <View>
+                <Text className="text-center mt-3" style={globalStyles.text}>React Native & Expo</Text>
+            </View>
     </View>
   )
 }
